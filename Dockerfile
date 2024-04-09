2860a6e071f99be451c417135605ded501592686
Kime <gemex18@gmail.com>
2024-04-09 15:32:25 +0300

    Add method

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,4 @@ COPY . .
 
 RUN pip install -r requirements.txt
 
-# CMD ["uvicorn", "app.main:app", "--reload", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "app.main:app", "--reload", "--host", "0.0.0.0", "--port", "80"]
--- app/config.py
+++ app/config.py
@@ -9,6 +9,7 @@ DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
+DB_NAME = os.environ.get("DB_CONTAINER")
 
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
