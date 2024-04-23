5c368a86e89d72c03731884430dc14317135e17d
Kime <gemex18@gmail.com>
2024-04-23 16:18:57 +0300

    Crete models & fix socket router

--- app/config.py
+++ app/config.py
@@ -9,10 +9,3 @@ DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
-# DB_NAME = os.environ.get("DB_HOST_CONTAINER")
-# DB_NAME = os.environ.get("DB_PORT_CONTAINER")
-
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
-ALGORITHM = os.environ.get("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
